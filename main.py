b92a742aa67b3f3f7ae1a6b406293b6e0ec5c52b
akisblack <76808208+AlexBlackeyed@users.noreply.github.com>
2021-07-08 20:21:22 +0300

    Added a scoring system
    Now each game is a best of three rounds and at every mini-game the amount of wins needed for the player and com to win are announced.

--- main.py
+++ main.py
@@ -1,53 +1,118 @@
 import random
-
-rps = ("rock", "paper", "scissors")
-
-
-computer = random.choice(rps)
-yes = ("YES","Y", "y", "yes")
+from time import sleep
+rps = ("rock", "paper", "scissors") #tuple of the possible inputs for the player.
+yes = ("YES","Y", "y", "yes") #tuple for all the possible ways to say yes or no when asked to.
 no = ("NO", "no", "n", "N")
 
-def main_func():
-    while True:
-        computer = random.choice(rps)
-        break
-    player = input("Rock,Paper or Scissors?\n>")
-    print(computer)
-    while True:
+
+
+def main_func(): 
+    com_wins = 0 
+    player_wins = 0
+    player_remaining_wins = 3 - player_wins
+    com_remaining_wins = 3 - com_wins
+    while True: #Game Loop starts here.
+        player = input("Rock,Paper or Scissors?\n>")
+        computer = random.choice(rps)#Makes random choice out of the previous rps variable.
+        print(computer)
         if player == computer:
-            print("Tie")
+            print("Tie. COM needs {} more wins to win while Player needs {} more wins to win!!".format(com_remaining_wins, player_remaining_wins))
+            continue
         elif player == "paper" and computer == "rock":
-            print("Player won")
+            player_wins += 1
+            player_wins
+            player_remaining_wins = 3 - player_wins #defines how the variable is calculated.
+            if player_remaining_wins == 0:
+                print("Player won!!!")
+                com_wins = 0
+                player_wins = 0
+                player_remaining_wins = 3 - player_wins
+                com_remaining_wins = 3 - com_wins
+            else:
+                print("Player won and now has {} wins!! {} more to win".format(player_wins, player_remaining_wins))
+                continue
         elif player == "rock" and computer == "scissors":
-            print("Player won")
+            player_wins += 1
+            player_wins
+            player_remaining_wins = 3 - player_wins
+            if player_remaining_wins == 0:
+                print("Player won!!!")
+                com_wins = 0
+                player_wins = 0
+                player_remaining_wins = 3 - player_wins
+                com_remaining_wins = 3 - com_wins
+            else:
+                print("Player won and now has {} wins!! {} more to win".format(player_wins, player_remaining_wins))
+                continue
         elif player == "scissors" and computer == "paper":
-            print("Player won")
+            player_wins += 1
+            player_wins
+            player_remaining_wins = 3 - player_wins
+            if player_remaining_wins == 0:
+                print("Player won!!!")
+                com_wins = 0
+                player_wins = 0
+                player_remaining_wins = 3 - player_wins
+                com_remaining_wins = 3 - com_wins      
+            else:
+                print("Player won and now has {} wins!! {} more to win".format(player_wins, player_remaining_wins))
+                continue
         elif computer == "paper" and player == "rock":
-            print("COM won")
+            com_wins += 1
+            com_wins
+            com_remaining_wins = 3 - com_wins
+            if com_remaining_wins == 0:
+                print("COM won!!!")
+                com_wins = 0
+                player_wins = 0
+                player_remaining_wins = 3 - player_wins
+                com_remaining_wins = 3 - com_wins
+                
+            else:
+                print("COM won and now has {} wins!! {} more to win".format(com_wins, com_remaining_wins))
+                continue
         elif computer == "rock" and player == "scissors":
-            print("COM won")
+            com_wins
+            com_wins += 1
+            com_remaining_wins = 3 - com_wins
+            if com_remaining_wins == 0:
+                print("COM won!!!")
+                com_wins = 0
+                player_wins = 0
+                player_remaining_wins = 3 - player_wins
+                com_remaining_wins = 3 - com_wins
+                
+            else:
+                print("COM won and now has {} wins!! {} more to win".format(com_wins, com_remaining_wins))
+                continue
+
         elif computer == "scissors" and player == "paper":
-            print("COM won")
+            com_wins
+            com_wins += 1
+            com_remaining_wins = 3 - com_wins
+            if com_remaining_wins == 0:
+                print("COM won!!!")
+                com_wins = 0
+                player_wins = 0
+                player_remaining_wins = 3 - player_wins
+                com_remaining_wins = 3 - com_wins
+                
+            else:
+                print("COM won and now has {} wins!! {} more to win".format(com_wins, com_remaining_wins))
+                continue
         else:
             print("I dont think you have the facilities for that big man")
             answer_3 = str(input("Do you want to play again?[y/n]\n>"))
             if answer_3 in yes:
-                main_func()
+                continue
             elif answer_3 in no:
                 quit()
-        break
-    answer_2 = str(input("Do you want to play again?[y/n]\n>"))
-    if answer_2 in yes:
-        main_func()
-    elif answer_2 in no:
-        quit()
-    
+                sleep(2)
+        answer2 = input("Do you want to play again?[y/n]")
+        if answer2 in yes:
+            continue
+        else:
+            sleep(2)
+            quit()    
 
 main_func()
-
-
-
-
-
-
-
